1a0bd9f1485783bafab7692cfc86eca5a1aeaf77
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2025-09-17 17:57:22 -0400

    textToSpeech function now returns an HTMLAudioElement for reusability

--- rise-dc-app/src/shared/utils/textToSpeech.ts
+++ rise-dc-app/src/shared/utils/textToSpeech.ts
@@ -1,22 +1,29 @@
-import { SpeechConfig, AudioConfig, SpeechSynthesizer, ResultReason, SpeechSynthesisResult } from "microsoft-cognitiveservices-speech-sdk";
+import { SpeechConfig, AudioConfig, SpeechSynthesizer, ResultReason, SpeechSynthesisResult, AudioOutputStream } from "microsoft-cognitiveservices-speech-sdk";
 
-export async function textToSpeech(text: string, _filename?: string) {
+export async function getAudioElementFromText(text: string): Promise<HTMLAudioElement> {
   if (!import.meta.env.VITE_SPEECH_KEY || !import.meta.env.VITE_SPEECH_REGION) {
     throw new Error("Missing credentials for text-to-speech")
   }
   const speechConfig = SpeechConfig.fromSubscription(import.meta.env.VITE_SPEECH_KEY, import.meta.env.VITE_SPEECH_REGION);
-
-  const audioConfig = AudioConfig.fromDefaultSpeakerOutput();
+  const stream = AudioOutputStream.createPullStream();
+  const audioConfig = AudioConfig.fromStreamOutput(stream);
 
   speechConfig.speechSynthesisVoiceName = "en-US-AvaMultilingualNeural";
 
   const synthesizer: SpeechSynthesizer | null = new SpeechSynthesizer(speechConfig, audioConfig);
-  synthesizer.speakTextAsync(text, (e: SpeechSynthesisResult) => {
-    if (e.reason === ResultReason.SynthesizingAudioCompleted) {
-      console.log('done creating audio file')
-    } else {
-      console.log('speech synthesis canceled');
-    }
-    synthesizer.close();
-  }, (error: string) => console.error(error));
+  return new Promise((resolve, reject) => {
+    synthesizer.speakTextAsync(text, (e: SpeechSynthesisResult) => {
+      if (e.reason === ResultReason.SynthesizingAudioCompleted) {
+        const blob = new Blob([e.audioData], { type: "audio/wav" });
+        const audio = new Audio(URL.createObjectURL(blob));
+        audio.play();
+        console.log('done creating audio file')
+        resolve(audio);
+      } else {
+        console.log('speech synthesis canceled');
+        reject('speech synthesis canceled');
+      }
+      synthesizer.close();
+    }, (error: string) => console.error(error));
+  });
 }
