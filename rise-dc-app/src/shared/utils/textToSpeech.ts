4728a2b57378cc99697057e03708ef35e4ce1202
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2025-09-17 16:29:40 -0400

    wrote text-to-speech function (currently doesn't save file)

---
+++ rise-dc-app/src/shared/utils/textToSpeech.ts
@@ -0,0 +1,21 @@
+import { SpeechConfig, AudioConfig, SpeechSynthesizer, ResultReason, SpeechSynthesisResult } from "microsoft-cognitiveservices-speech-sdk";
+
+export async function textToSpeech(text: string, filename: string) {
+  if (!import.meta.env.VITE_SPEECH_KEY || !import.meta.env.VITE_SPEECH_REGION) {
+    throw new Error("Missing credentials for text-to-speech")
+  }
+  const speechConfig = SpeechConfig.fromSubscription(import.meta.env.VITE_SPEECH_KEY, import.meta.env.VITE_SPEECH_REGION);
+  const audioConfig = AudioConfig.fromAudioFileOutput(filename);
+
+  speechConfig.speechSynthesisVoiceName = "en-US-AvaMultilingualNeural";
+
+  const synthesizer: SpeechSynthesizer | null = new SpeechSynthesizer(speechConfig, audioConfig);
+  synthesizer.speakTextAsync(text, (e: SpeechSynthesisResult) => {
+    if (e.reason === ResultReason.SynthesizingAudioCompleted) {
+      console.log('done creating audio file')
+    } else {
+      console.log('speech synthesis canceled');
+    }
+    synthesizer.close();
+  }, (error: string) => console.error(error));
+}
