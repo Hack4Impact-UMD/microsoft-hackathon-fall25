1626633c4ecb59abfab66728f34eb5cb1b69cd9e
krishnan mac <inbantholkappian@gmail.com>
2025-09-17 18:12:15 -0400

    installed tailwind

--- rise-dc-app/src/pages/Home.tsx
+++ rise-dc-app/src/pages/Home.tsx
@@ -3,7 +3,7 @@ import styles from './Page.module.css'
 export default function Home() {
   return (
     <div className={styles.container}>
-      <h1>RISE DC</h1>
+      <h1 className='text-3xl'>RISE DC</h1>
       <p>Welcome to RISE DC</p>
     </div>
   )
