5fef3ad3297644ee945a1c6165eaa053e6edb235
Steven Ha <109867418+stevenha75@users.noreply.github.com>
2025-09-17 13:26:47 -0400

    Adding cookbook types

---
+++ rise-dc-app/src/shared/types/index.ts
@@ -0,0 +1,75 @@
+// Cookbook Types
+
+export interface Recipe {
+  id: string;
+  user_id?: string;
+  title: string;
+  noCook: boolean;
+  time: string;
+  servingSize: number;
+  meal: ('snack' | 'dessert' | 'breakfast' | 'lunch' | 'dinner')[];
+  ingredients: RecipeIngredient[];
+  tools: Tool[];
+  utensils: Utensil[];
+  instructions: Instruction[];
+  nutrition: Nutrition[];
+}
+
+export interface RecipeIngredient {
+  ingredient: Ingredient;
+  quantity: string;
+}
+
+export interface Ingredient {
+  id: string;
+  name: string;
+  image_id: string;
+}
+
+export interface Tool {
+  id: string;
+  name: string;
+  image_id: string;
+}
+
+export interface Utensil {
+  id: string;
+  name: string;
+  image_id: string;
+}
+
+export interface Image {
+  id: string;
+  caption: string;
+}
+
+export interface MealPrep {
+  id: string;
+  week: string;
+  recipes: Recipe[];
+}
+
+export interface GroceryList {
+  id: string;
+  week: string; // set of dates
+  ingredients: GroceryListIngredient[];
+}
+
+export interface GroceryListIngredient {
+  ingredient: Ingredient;
+  quantity: string;
+  purchased_status: boolean;
+}
+
+export interface Nutrition {
+  number: number;
+  field: string;
+}
+
+export interface Instruction {
+  id: string;
+  step_number: number;
+  instructions: string;
+  text_instructions: string;
+  image_id?: string;
+}
