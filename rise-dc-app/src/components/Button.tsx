90ebb7846aa056b58832f121cf9446f9baf06b7b
ArnavGupta23 <arnavkgu@gmail.com>
2025-09-17 19:08:11 -0400

    updated to get rid of centering

--- rise-dc-app/src/components/Button.tsx
+++ rise-dc-app/src/components/Button.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-// Reusable Tailwind button component that centers horizontally on the page
+// Reusable Tailwind button component
 type ButtonProps = {
   text: string;
   onClick?: () => void;
@@ -9,14 +9,12 @@ type ButtonProps = {
 
 const Button: React.FC<ButtonProps> = ({ text, onClick, className }) => {
   return (
-    <div className="flex justify-center items-center mt-6">
       <button
         onClick={onClick}
         className={`px-8 py-3 rounded-xl bg-gray-700 text-white font-semibold shadow-md hover:bg-gray-800 transition duration-200 ${className}`}
         >
         {text}
       </button>
-    </div>
   );
 };
 
