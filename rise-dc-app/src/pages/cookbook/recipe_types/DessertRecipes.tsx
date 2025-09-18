f7d755dcadd0ad8d6e4c08ed98dd0c66b54e6d53
ArnavGupta23 <arnavkgu@gmail.com>
2025-09-18 16:56:21 -0400

    recipe types updated

--- rise-dc-app/src/pages/cookbook/recipe_types/DessertRecipes.tsx
+++ rise-dc-app/src/pages/cookbook/recipe_types/DessertRecipes.tsx
@@ -1,63 +1,48 @@
-import { useNavigate } from "react-router"; 
-import ImageCardStar from '../../../shared/components/ImageCardStar';
-import Slideshow from '../../../shared/components/Slideshow';
+import { useNavigate } from "react-router";
+
+import ImageCardStar from "../../../shared/components/ImageCardStar";
+import Slideshow from "../../../shared/components/Slideshow";
+import { recipesByCategory } from "../../../shared/data/dummyRecipes";
 
 export default function DessertRecipes() {
-    const navigate = useNavigate();
-    const placeholder = "https://www.cranfield-colours.co.uk/wp-content/uploads/2022/01/cranfield-traditional-etching-ink-mid-black-400x221.jpg";
+  const navigate = useNavigate();
+  const dessertRecipes = recipesByCategory.dessert || [];
 
-    return (
-        <div className="p-10 flex flex-col space-y-5">
-            <button
-                onClick={() => navigate({pathname: '/cookbook/all-recipes'})}
-            >
-                <svg width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path fill-rule="evenodd" clip-rule="evenodd" d="M36.6654 34.0297C32.5876 29.0519 28.9665 26.2275 25.802 25.5563C22.6376 24.8852 19.6248 24.7838 16.7637 25.2522V34.1663L3.33203 19.6205L16.7637 5.83301V14.3055C22.0543 14.3472 26.552 16.2452 30.257 19.9997C33.9615 23.7541 36.0976 28.4308 36.6654 34.0297Z" fill="black" stroke="black" stroke-width="3.33333" stroke-linejoin="round"/>
-                </svg>
-            </button>
+  return (
+    <div className="p-10 flex flex-col space-y-5">
+      <button onClick={() => navigate({ pathname: "/cookbook/all-recipes" })}>
+        <svg
+          width="40"
+          height="40"
+          viewBox="0 0 40 40"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            fillRule="evenodd"
+            clipRule="evenodd"
+            d="M36.6654 34.0297C32.5876 29.0519 28.9665 26.2275 25.802 25.5563C22.6376 24.8852 19.6248 24.7838 16.7637 25.2522V34.1663L3.33203 19.6205L16.7637 5.83301V14.3055C22.0543 14.3472 26.552 16.2452 30.257 19.9997C33.9615 23.7541 36.0976 28.4308 36.6654 34.0297Z"
+            fill="black"
+            stroke="black"
+            strokeWidth="3.33333"
+            strokeLinejoin="round"
+          />
+        </svg>
+      </button>
 
-            <Slideshow 
-                title="Dessert" 
-                images={[
-                <ImageCardStar
-                    key="d1"
-                    src={placeholder}
-                    caption="Meal Title"
-                    defaultFavorite={false}
-                />,
-                <ImageCardStar
-                    key="d2"
-                    src={placeholder}
-                    caption="Meal Title"
-                    defaultFavorite={false}
-                />,
-                <ImageCardStar
-                    key="d3"
-                    src={placeholder}
-                    caption="Meal Title"
-                    defaultFavorite={false}
-                />,
-                <ImageCardStar
-                    key="d4"
-                    src={placeholder}
-                    caption="Meal Title"
-                    defaultFavorite={false}
-                />,
-                <ImageCardStar
-                    key="d5"
-                    src={placeholder}
-                    caption="Meal Title"
-                    defaultFavorite={false}
-                />,
-                <ImageCardStar
-                    key="d6"
-                    src={placeholder}
-                    caption="Meal Title"
-                    defaultFavorite={false}
-                />
-                ]}
-                imagesPerRow={2} 
-            />
-        </div>
-    );
+      <Slideshow
+        title="Dessert"
+        images={dessertRecipes.map((recipe) => (
+          <ImageCardStar
+            key={recipe.id}
+            recipeId={recipe.id}
+            src={recipe.image}
+            caption={recipe.title}
+            category={recipe.category}
+          />
+        ))}
+        imagesPerRow={2}
+      />
+    </div>
+  );
 }
--- rise-dc-app/src/pages/cookbook/recipe_types/LunchDinnerRecipes.tsx
+++ rise-dc-app/src/pages/cookbook/recipe_types/LunchDinnerRecipes.tsx
@@ -1,63 +1,48 @@
-import { useNavigate } from "react-router"; 
-import ImageCardStar from '../../../shared/components/ImageCardStar';
-import Slideshow from '../../../shared/components/Slideshow';
+import { useNavigate } from "react-router";
+
+import ImageCardStar from "../../../shared/components/ImageCardStar";
+import Slideshow from "../../../shared/components/Slideshow";
+import { recipesByCategory } from "../../../shared/data/dummyRecipes";
 
 export default function LunchDinnerRecipes() {
-    const navigate = useNavigate();
-    const placeholder = "https://www.cranfield-colours.co.uk/wp-content/uploads/2022/01/cranfield-traditional-etching-ink-mid-black-400x221.jpg";
+  const navigate = useNavigate();
+  const lunchDinnerRecipes = recipesByCategory.lunchDinner || [];
 
-    return (
-        <div className="p-10 flex flex-col space-y-5">
-            <button
-                onClick={() => navigate({pathname: '/cookbook/all-recipes'})}
-            >
-                <svg width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path fill-rule="evenodd" clip-rule="evenodd" d="M36.6654 34.0297C32.5876 29.0519 28.9665 26.2275 25.802 25.5563C22.6376 24.8852 19.6248 24.7838 16.7637 25.2522V34.1663L3.33203 19.6205L16.7637 5.83301V14.3055C22.0543 14.3472 26.552 16.2452 30.257 19.9997C33.9615 23.7541 36.0976 28.4308 36.6654 34.0297Z" fill="black" stroke="black" stroke-width="3.33333" stroke-linejoin="round"/>
-                </svg>
-            </button>
+  return (
+    <div className="p-10 flex flex-col space-y-5">
+      <button onClick={() => navigate({ pathname: "/cookbook/all-recipes" })}>
+        <svg
+          width="40"
+          height="40"
+          viewBox="0 0 40 40"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            fillRule="evenodd"
+            clipRule="evenodd"
+            d="M36.6654 34.0297C32.5876 29.0519 28.9665 26.2275 25.802 25.5563C22.6376 24.8852 19.6248 24.7838 16.7637 25.2522V34.1663L3.33203 19.6205L16.7637 5.83301V14.3055C22.0543 14.3472 26.552 16.2452 30.257 19.9997C33.9615 23.7541 36.0976 28.4308 36.6654 34.0297Z"
+            fill="black"
+            stroke="black"
+            strokeWidth="3.33333"
+            strokeLinejoin="round"
+          />
+        </svg>
+      </button>
 
-            <Slideshow 
-                title="Lunch/Dinner" 
-                images={[
-                <ImageCardStar
-                    key="ld1"
-                    src={placeholder}
-                    caption="Meal Title"
-                    defaultFavorite={false}
-                />,
-                <ImageCardStar
-                    key="ld2"
-                    src={placeholder}
-                    caption="Meal Title"
-                    defaultFavorite={false}
-                />,
-                <ImageCardStar
-                    key="ld3"
-                    src={placeholder}
-                    caption="Meal Title"
-                    defaultFavorite={false}
-                />,
-                <ImageCardStar
-                    key="ld4"
-                    src={placeholder}
-                    caption="Meal Title"
-                    defaultFavorite={false}
-                />,
-                <ImageCardStar
-                    key="ld5"
-                    src={placeholder}
-                    caption="Meal Title"
-                    defaultFavorite={false}
-                />,
-                <ImageCardStar
-                    key="ld6"
-                    src={placeholder}
-                    caption="Meal Title"
-                    defaultFavorite={false}
-                />
-                ]}
-                imagesPerRow={2} 
-            />
-        </div>
-    );
+      <Slideshow
+        title="Lunch/Dinner"
+        images={lunchDinnerRecipes.map((recipe) => (
+          <ImageCardStar
+            key={recipe.id}
+            recipeId={recipe.id}
+            src={recipe.image}
+            caption={recipe.title}
+            category={recipe.category}
+          />
+        ))}
+        imagesPerRow={2}
+      />
+    </div>
+  );
 }
