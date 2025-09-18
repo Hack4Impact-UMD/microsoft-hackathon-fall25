51ab321bc5d39481f659b9a2778279089c86637e
Ryan Zhao <ryanzhao1105@gmail.com>
2025-09-17 23:21:46 -0400

    added progress bar

--- rise-dc-app/src/cookbook_components/ProgressBar.tsx
+++ rise-dc-app/src/cookbook_components/ProgressBar.tsx
@@ -3,22 +3,43 @@ import Button from "./Button";
 type ProgressBarProps = {
   currentStep: number;
   totalSteps: number;
-  onClick: () => void;
+  onNext: () => void;
 };
 
-const ProgressBar = ({
-  currentStep,
-  totalSteps,
-  onClick,
-}: ProgressBarProps) => {
+const ProgressBar = ({ currentStep, totalSteps, onNext }: ProgressBarProps) => {
   return (
     <div className="w-full max-w-lg mx-auto flex flex-col items-center">
+      <p className="text-lg font-semibold mb-2">
+        Step {currentStep}/{totalSteps}
+      </p>
+
+      <div className="w-full h-4 bg-[#E6E6E6] rounded-full flex  border-1 border-[#616161] overflow-hidden">
+        {/* Create progress bar based on length of recipe */}
+        {Array.from({ length: totalSteps }).map((_, index) => {
+          const isComplete = index + 1 <= currentStep;
+
+          return (
+            <div
+              key={index + 1}
+              className={`
+                h-full flex-1 transition-colors duration-300
+                ${isComplete ? "bg-[#0F6CBD]" : "bg-[#E6E6E6]"}
+                
+                // Divider between steps
+                ${!isComplete && index > 0 ? "border-l-1 border-[#616161]" : ""}
+              `}
+            />
+          );
+        })}
+      </div>
+
       {/* Next Step Button */}
-      <div className="mt-4">
-        {currentStep < totalSteps && (
-          <Button label="Next Step" onClick={onClick} />
+      <div className="mt-6">
+        {currentStep < totalSteps ? (
+          <Button label="Next Step" onClick={onNext} />
+        ) : (
+          <Button label="Done" />
         )}
-        {currentStep === totalSteps && <Button label="Finish" />}
       </div>
     </div>
   );
