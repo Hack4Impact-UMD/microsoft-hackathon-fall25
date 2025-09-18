2236e1e7f8eac8e73c77e998d6c60d2ffc616da0
nkhur <145411496+nkhur@users.noreply.github.com>
2025-09-18 17:07:07 -0400

    listscroll component: basic structure

---
+++ rise-dc-app/src/shared/components/ListScroll.tsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+
+type ListScrollProps<T extends { name: string }> = {
+  items: T[];
+};
+
+export default function ListScroll<T extends { name: string }>({ items }: ListScrollProps<T>) {
+  const [selected, setSelected] = useState<string>(items[0].name);
+
+  return (
+    <div className="flex flex-col gap-2">
+      <div className="border-gray-200 border-[1px] rounded-lg w-[230px] h-[282px] overflow-y-auto shadow-lg">
+        {items.map((item) => (
+          <div
+            key={item.name}
+            onClick={() => setSelected(item.name)}
+            className={`border-gray-200 border-t border-b p-3 cursor-pointer font-medium ${
+              selected.includes(item.name)
+                ? "bg-gray-200"
+                : "hover:bg-gray-100"
+            }`}
+          >
+            {item.name}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
