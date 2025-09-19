2a426dab0388d0261c0c7076deda6c8c500c25d0
nkhur <145411496+nkhur@users.noreply.github.com>
2025-09-18 21:54:50 -0400

    finished list scroll

--- rise-dc-app/src/pages/Cookbook.tsx
+++ rise-dc-app/src/pages/Cookbook.tsx
@@ -7,7 +7,6 @@ export default function Cookbook() {
     <div className={styles.container}>
       <h1>Adaptive Digital Cookbook</h1>
       <p>Digital cookbook for users with cognitive and literacy challenges</p>
-      <ListScroll items={[{name:"hi", id:"1", image_id:"1"}, {name:"hi", id:"1", image_id:"1"}, {name:"hi", id:"1", image_id:"1"}, {name:"hi", id:"1", image_id:"1"}, {name:"hi", id:"1", image_id:"1"}, {name:"hi", id:"1", image_id:"1"}, {name:"hi", id:"1", image_id:"1"}, {name:"hi", id:"1", image_id:"1"}]}/>
     </div>
 
 
--- rise-dc-app/src/shared/components/ListScroll.tsx
+++ rise-dc-app/src/shared/components/ListScroll.tsx
@@ -1,21 +1,21 @@
 import { useState } from "react";
 
-type ListScrollProps<T extends { name: string, id: string }> = {
+type ListScrollProps<T extends { name: string, id: string}> = {
   items: T[];
 };
 
 export default function ListScroll<T extends { name: string, id: string }>({ items }: ListScrollProps<T>) {
-  const [selected, setSelected] = useState<string>(items[0].id);
+  const [selected, setSelected] = useState<T>(items[0]);
 
   return (
     <div className="flex flex-col gap-2">
-      <div className="border-gray-200 border-[1px] rounded-lg w-[230px] h-[282px] overflow-y-auto shadow-lg">
+      <div className="border-gray-200 border-[1px] rounded-lg max-w-[230px] max-h-[282px] overflow-y-auto shadow-lg">
         {items.map((item) => (
           <div
             key={item.name}
-            onClick={() => setSelected(item.id)}
+            onClick={() => setSelected(item)}
             className={`border-gray-200 border-t border-b p-3 cursor-pointer font-medium ${
-              selected.includes(item.name)
+              selected.id == (item.id)
                 ? "bg-gray-200"
                 : "hover:bg-gray-100"
             }`}
