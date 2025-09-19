fa4c9afb66b266327588d42d021fba92286254b5
nkhur <145411496+nkhur@users.noreply.github.com>
2025-09-18 22:01:47 -0400

    merged w main

--- rise-dc-app/src/pages/Cookbook.tsx
+++ rise-dc-app/src/pages/Cookbook.tsx
@@ -4,6 +4,7 @@ import { Ingredient } from '../shared/types'
 
 export default function Cookbook() {
   return (
+    
     <div className={styles.container}>
       <h1>Adaptive Digital Cookbook</h1>
       <p>Digital cookbook for users with cognitive and literacy challenges</p>
