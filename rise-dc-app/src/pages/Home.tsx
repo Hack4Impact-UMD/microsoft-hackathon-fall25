f654c82efd2bf491601d08f96d8476246a1323ca
Lance <lanceu0128@gmail.com>
2025-09-20 23:25:03 -0400

    clean accidental home page commit

--- rise-dc-app/src/pages/Home.tsx
+++ rise-dc-app/src/pages/Home.tsx
@@ -1,4 +1,3 @@
-import { createEvent, listEvents } from '../services/service'
 import styles from './Page.module.css'
 
 export default function Home() {
@@ -6,9 +5,6 @@ export default function Home() {
     <div className={styles.container}>
       <h1 className='text-3xl'>RISE DC</h1>
       <p>Welcome to RISE DC</p>
-
-      <button onClick={() => console.log(listEvents())}>List Events</button>
-      <button onClick={() => createEvent({"name": "test event name"})}>Create Event</button>
     </div>
   )
 }
