f5d21165fabd33e238b1a52a2270abd9229f7fa2
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2025-09-20 22:18:11 -0400

    created useAllTasks & useCreateEvent hooks

---
+++ rise-dc-app/src/shared/hooks/useAllTasks.tsx
@@ -0,0 +1,9 @@
+import { useQuery } from "@tanstack/react-query";
+import { listTasks } from "../../services/service";
+
+export default function useAllTasks() {
+  return useQuery({
+    queryKey: ["tasks"],
+    queryFn: listTasks,
+  });
+}
