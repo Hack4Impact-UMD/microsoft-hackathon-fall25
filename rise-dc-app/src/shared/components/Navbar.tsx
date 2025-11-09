31f94b4a4f648fc5a0689d673c461b3e1b3ae565
Lance <lanceu0128@gmail.com>
2025-11-09 17:39:47 -0500

    migrate navbar from css modules to tailwind

--- rise-dc-app/src/shared/components/Navbar.tsx
+++ rise-dc-app/src/shared/components/Navbar.tsx
@@ -1,15 +1,17 @@
 import { Link } from 'react-router-dom'
-import styles from './Navbar.module.css'
 
 export default function Navbar() {
   return (
-    <nav className={styles.navbar}>
-      <Link to="/" className={styles.logo}>
-        <div className={styles.logoCircle}>
-          <img src={"/logo.png"} alt="Logo" className={styles.logoImg}/>
+    <nav className="flex gap-8 bg-white border-b border-gray-200 p-2.5">
+      <Link to="/" className="flex items-center">
+        <div className="w-15 h-15 rounded-full border-2 border-[#B53E03] bg-white flex items-center justify-center mr-2">
+          <img
+            src="/logo.png"
+            alt="Logo"
+            className="w-11 h-11 object-contain"
+          />
         </div>
       </Link>
     </nav>
   )
 }
-
